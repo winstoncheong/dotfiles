69db312d4fda3d4870f8789438618eaa31a21a26
Winston Cheong <cheong.winston1337@gmail.com>
2015-12-06 22:52:00 +0000

    Moved vim installation lines to make sense. Should always run BundleInstall, in case vimrc changes between commits.

--- install.sh
+++ install.sh
@@ -4,14 +4,14 @@ DIR=$(cd $(dirname ${0}) && pwd)
 # set up vim
 if [[ ! -e ~/.vim/bundle/Vundle.vim ]]; then
 	git clone http://github.com/gmarik/Vundle.vim ~/.vim/bundle/Vundle.vim
-	vim +PluginInstall +qall # Does the plugin installation
 fi
-
-# set up git
-"$DIR"/git-setup
 
 if [[ -e $HOME/.vimrc ]]; then
 	mv $HOME/.vimrc $HOME/.vimrc.bak
 fi
 
 ln -s "$DIR"/vimrc $HOME/.vimrc
+vim +PluginInstall +qall # Does the plugin installation
+
+# set up git
+"$DIR"/git-setup
